b98a57d115d737b4a35eefc5f52dfd6f32fef034
Oldrich Kruchna <kruchna.o@gmail.com>
2017-07-05 13:59:44 +0200

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  }
+}
